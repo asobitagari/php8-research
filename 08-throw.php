5c543225a9379ed05a281b1e4b3e1779fc5f9e65
Kimihisa Ishizaki <asobitagari@asobitagari.xyz>
2020-11-29 13:41:20 +0900

    throw

--- 08-throw.php
+++ 08-throw.php
@@ -2,13 +2,4 @@
 
 false ?: throw Exception;
 
-// $fn = fn() => throw new Exception('mogemoge');
-
-
-function aaa(mixed $a)
-{
-    var_dump($a);
-}
-
-
-aaa("mogemoge");
+$fn = fn() => throw new Exception('mogemoge');
